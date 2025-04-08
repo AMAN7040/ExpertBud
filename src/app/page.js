8a271a2d1bf3525ad71fe53574c090ee5141cb13
AMAN7040 <amansingh79620@gmail.com>
2025-04-08 15:50:03 +0530

    feat(home page): create fully optimized homepage with metadata, responsive design

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,32 @@
-import Image from "next/image";
 
+export const metadata = {
+  title: "Home - Learning Starts Here",
+  description:
+    "Welcome to ExpertBuddy — Accelerate your skills with ExpertBuddy’s curated resources, expert mentors, and real-time collaboration tools.",
+  openGraph: {
+    title: "ExpertBuddy | Personalized Learning Starts Here",
+    description:
+      "Get started with curated mentorship and expert-driven guidance.",
+    url: "",
+    siteName: "ExpertBuddy",
+    type: "website",
+  },
+  twitter: {
+    title: "Personalized Learning Starts Here",
+    description: "Get expert mentorship and start growing today.",
+  },
+};
 export default function Home() {
   return (
-   <div>
-    Initiated the project
-   </div>
+    <main>
+      <section>
+        <h1>
+          Welcome to <span>ExpertBuddy</span>
+        </h1>
+        <p>
+          Accelerate your journey with real-world assignments and modern tech.
+        </p>
+      </section>
+    </main>
   );
 }
