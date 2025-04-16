c4b897f107cd8cf55af4765c9f35e514493c91bb
AMAN7040 <amansingh79620@gmail.com>
2025-04-16 22:33:55 +0530

    feat: add component with business image

---
+++ src/app/components/Homework/Section-1/BusinessSec.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+
+const BusinessSec = () => {
+  return (
+    <>
+      <Image src="/Business.png" fill alt="Busineess Section picture"/>
+    </>
+  );
+};
+
+export default BusinessSec;
