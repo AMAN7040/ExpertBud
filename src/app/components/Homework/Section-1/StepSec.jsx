b29513046be1885b876e40e107ea6894cc896f26
AMAN7040 <amansingh79620@gmail.com>
2025-04-16 23:14:53 +0530

    feat: add component in Herosection

---
+++ src/app/components/Homework/Section-1/StepSec.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const StepSec = () => {
+  return (
+    <>
+      <div className="bg-white rounded-[50px]  md:rounded-[100px] w-fit h-fit min-h-8 shadow-[0px_2px_8px_#0000000A] box-border p-2">
+        <p className="font-semibold text-dark font-dm-sans  text-[clamp(10px,1.2vw,14px)]">
+          How it works
+        </p>
+      </div>
+      <div className="bg-white rounded-[50px]  md:rounded-[100px] w-fit h-fit min-h-8 shadow-[0px_2px_8px_#0000000A] box-border  p-2">
+        <p className="font-normal text-[#6B7B93] font-dm-sans  text-[clamp(10px,1.2vw,14px)]">
+          Find an Expert
+        </p>
+      </div>
+      <div className="bg-white rounded-[50px]  md:rounded-[100px] w-fit h-fit min-h-8 shadow-[0px_2px_8px_#0000000A]  box-border p-2">
+        <p className="font-normal text-[#6B7B93] font-dm-sans  text-[clamp(10px,1.2vw,14px)]">
+          Featured
+        </p>
+      </div>
+      <div className="bg-white rounded-[50px]  md:rounded-[100px] w-fit h-fit min-h-8 shadow-[0px_2px_8px_#0000000A] box-border  p-2">
+        <p className="font-normal text-[#6B7B93] font-dm-sans  text-[clamp(10px,1.2vw,14px)]">
+          Claim Offers
+        </p>
+      </div>
+      <div className="bg-white rounded-[50px]  md:rounded-[100px] w-fit h-fit min-h-8 shadow-[0px_2px_8px_#0000000A] box-border  p-2">
+        <p className="font-normal text-[#6B7B93] font-dm-sans  text-[clamp(10px,1.2vw,14px)]">
+          FAQs
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default StepSec;
