f624c22b321374a7e3affc25e654eac70e9b08d5
AMAN7040 <amansingh79620@gmail.com>
2025-04-16 23:43:37 +0530

    feat: initialized Details Section in Homework page

---
+++ src/app/components/Homework/Section-2/DetailsSection.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const DetailsSection = () => {
+  return (
+    <div className="flex justify-center ">
+      <div
+        className=" mx-auto w-[80vw] max-w-[1170px]
+    px-6 md:px-[24px] 
+    pt-[36px] pb-[36px]
+    flex justify-between
+    rounded-[16px]
+    border-t-[1.11px] border-r-[1.11px] border-b-[8.9px] border-l-[1.11px]
+    border-[#FFFFFF]
+    bg-[#FFFFFF]"
+      >
+        <div>first</div>
+        <div>Scond</div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailsSection;
