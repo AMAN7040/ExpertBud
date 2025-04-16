27bb0efd08818f02047cb7c6e5da731ad88243da
AMAN7040 <amansingh79620@gmail.com>
2025-04-16 23:32:47 +0530

    fix: adjust fixed navbar layering and layout offset for content visibility

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import Hamburger from "./Hamburger";
 
 const Navbar = () => {
   return (
-    <header className="fixed top-0 left-0 w-full h-17.5 border border-secondary z-30 ">
-      <nav className="flex justify-between items-center h-[70px] w-full px-[clamp(4px,6vw,8%)]">
+    <header className="fixed top-0 left-0 w-full h-17.5 border bg-white/50 backdrop-blur-md border-b shadow-[0_2px_8px_#0000000A] border-secondary z-30 ">
+      <nav className="flex justify-between items-center h-[70px] w-full px-[clamp(4px,6vw,8%)] z-30">
         <div className="flex justify-between space-x-5">
           <Hamburger />
           <Logo />
