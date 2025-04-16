1e0764c75619eeb3e22684b8ddcd10573f29bbb1
AMAN7040 <amansingh79620@gmail.com>
2025-04-16 22:37:02 +0530

    feat: initialized /homework  page as per design specifications with heroSection

---
+++ src/app/homework/page.js
@@ -0,0 +1,14 @@
+import HeroSection from "../components/Homework/Section-1/HeroSection";
+
+export const metadata = {
+  title: "Homework Process",
+  description: "Start the process and get to know everything about the process",
+};
+
+export default function HomeworkPage() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
